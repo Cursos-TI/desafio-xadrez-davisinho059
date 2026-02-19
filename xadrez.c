3f723a7dc3bbd0a39d57211e184ec6facb763514
davisinho059 <davisinho059@gmail.com>
2026-02-19 23:49:05 +0000

    adicionar novo loop aninhado para movimento do cavalo

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,9 @@ int main() {
     int bispo = 0;
     int torre = 1;
     int rainha = 1;
-    
+    char cavalo = 0;
+    char movimentocompleto= 1;//flag para controlar o movimento em 'L'
+
     printf("movimentos das peças de xadrez\n");
     
     // BISPO com FOR 5 casas na diagonal cima-direita
@@ -23,7 +25,16 @@ int main() {
         printf("rainha, esquerda %d\n",rainha);
         rainha ++;
  } while(rainha <= 8);
+   printf("\n"); //Separa o movimento do Cavalo dos movimentos anteriores com uma linha em branco.
 
+// cavalo com for (direçao formato L)
+ while (movimentocompleto--)
+  {
+     for (cavalo = 0; cavalo < 2; cavalo++) {
+          printf("L,baixo\n"); // imprime baixo duas vezes
+     }
+     printf("L,esquerda\n"); // imprime "esquerda" uma vez
+     }
   return 0;
 }
 
